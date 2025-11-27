a2f52a3bf25c6e4b21260dec692bf9ca60cf1ec6
antoniorb1913 <antoniorb1913@gmail.com>
2025-11-27 22:33:47 +0100

    todo

--- fronted/index.php
+++ fronted/index.php
@@ -1,7 +1,7 @@
 <?php
 
 $api_url = getenv('API_URL') ?: 'https://https://pets-app-2.onrender.com/pet/list';
-$data = file_get_contents($url);
+$data = file_get_contents($api_url);
 //Json to Array
 $pets = json_decode($data, true);
 
